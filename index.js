591357da6082f3e8a0283774f4727f0fbef50fa6
Dolores-She <106836300+Dolores-She@users.noreply.github.com>
2022-08-24 18:35:10 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+    // Задание 1:
+
+
+  let city = "Ufa";
+  console.log(city);
+  let country = "Russia";
+  console.log(country);
+  let population = 1000075;
+  console.log(population);
+  const bstadium = new Boolean ("true");
+  console.log("yes" + " " + bstadium);
+
+ 
+    // Задание 2:
+    
+    let a = 40;
+    let b = 70;
+    let c = "cm";
+    console.log(a *= b);
+    console.log(2800 + c);
+
+
+    // Задание 3:
+    
+
+      let x = 2, time;
+      let y = 95, speedOfFirst;
+      let s = 114, speedOfSecond;
+
+   console.log(y * x);
+   console.log(s * x);
+   console.log(190 + 228);
+   console.log(228 + "km");
+
+
+    // Задание 4:
+
+  const randomNumber = Math.floor(Math.random() * 100);
+
+  if(randomNumber < 20) {
+       console.log("randomNumber меньше 20");
+  } else if (randomNumber > 50) {
+       console.log ("randomNumber больше 50");
+  } else {
+       console.log ("randomNumber больше 20, и меньше 50");
+  }
+
+    // Задание 5:
+    
+const randomNumber1 = Math.floor(Math.random() * 100);
+
+switch (true) {
+   case  randomNumber < 20:
+       console.log("randomNumber1 меньше 20");
+       break; 
+   case randomNumber1 > 50:
+       console.log("randomNumber1 больше 50");
+      break;
+   default:
+       console.log("randomNumber1 больше 20, и меньше 50");
+      break;
+ }
+
+
